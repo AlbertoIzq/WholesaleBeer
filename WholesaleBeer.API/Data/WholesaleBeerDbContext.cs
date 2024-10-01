3ab0ebd462d7d1681add61be62bcee8a4192e701
Alberto <alberto.izquierdomo@gmail.com>
2024-10-02 01:11:23 +0200

    Add DbContext

---
+++ WholesaleBeer.API/Data/WholesaleBeerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WholesaleBeer.API.Models.Domain;
+
+namespace WholesaleBeer.API.Data
+{
+    public class WholesaleBeerDbContext : DbContext
+    {
+        public WholesaleBeerDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brewery> Breweries { get; set; }
+        public DbSet<Wholesaler> Wholesalers { get; set; }
+        public DbSet<BeerStock> BeerStocks { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
