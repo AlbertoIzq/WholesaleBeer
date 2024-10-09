ff857182e867df6386beb436588de3c5e8652379
Alberto <alberto.izquierdomo@gmail.com>
2024-10-09 19:08:54 +0200

    Extract ValidateOrderDetail method from OrderDetailsController into a separate class

--- WholesaleBeer.API/Controllers/OrderDetailsController.cs
+++ WholesaleBeer.API/Controllers/OrderDetailsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using WholesaleBeer.API.CustomActionFilters;
+using WholesaleBeer.API.Helpers;
 using WholesaleBeer.API.Models.Domain;
 using WholesaleBeer.API.Models.DTO;
 using WholesaleBeer.API.Repositories.Interfaces;
@@ -19,6 +20,7 @@ namespace WholesaleBeer.API.Controllers
         private readonly IWholesalerRepository _wholesalerRepository;
 
         private readonly OrderDetailPriceCalculator _orderDetailPriceCalculator;
+        private readonly OrderDetailValidator _orderDetailValidator;
         private readonly IBeerRepository _beerRepository;
 
         public OrderDetailsController(IOrderDetailRepository orderDetailRepository,
@@ -32,6 +34,8 @@ namespace WholesaleBeer.API.Controllers
 
             _beerRepository = beerRepository;
             _orderDetailPriceCalculator = new OrderDetailPriceCalculator(_beerRepository);
+            _orderDetailValidator = new OrderDetailValidator(_wholesalerRepository,
+                _orderDetailRepository, _beerStockRepository);
         }
 
         // CREATE OrderDetail
@@ -41,7 +45,7 @@ namespace WholesaleBeer.API.Controllers
         {
             try
             {
-                await ValidateOrderDetail(addOrderDetailRequestDto);
+                await _orderDetailValidator.ValidateOrderDetail(addOrderDetailRequestDto);
 
                 // Map DTO to Domain Model
                 var orderDetailDomainModel = _mapper.Map<OrderDetail>(addOrderDetailRequestDto);
@@ -62,48 +66,6 @@ namespace WholesaleBeer.API.Controllers
             catch (Exception ex)
             {
                 return Problem(ex.Message, null, (int)HttpStatusCode.BadRequest);
-            }
-        }
-
-        private async Task ValidateOrderDetail(AddOrderDetailRequestDto addOrderDetailRequestDto)
-        {
-            // Check if order is empty
-            if (addOrderDetailRequestDto.Quantity <= 0)
-            {
-                throw new Exception("The order cannot be empty");
-            }
-
-            // Check if wholesaler exists
-            var wholesalers = await _wholesalerRepository.GetAllAsync();
-            var wholesaler = wholesalers.FirstOrDefault(x => x.Id == addOrderDetailRequestDto.WholesalerId);
-            if (wholesaler is null)
-            {
-                throw new Exception("The wholesaler must exist");
-            }
-
-            // Check if there's any duplicate in the order
-            var orderDetails = await _orderDetailRepository.GetAllAsync();
-            var orderDetail = orderDetails.FirstOrDefault(x => x.BeerId == addOrderDetailRequestDto.BeerId &&
-                x.WholesalerId == addOrderDetailRequestDto.WholesalerId && x.Quantity == addOrderDetailRequestDto.Quantity);
-            if (orderDetail is not null)
-            {
-                throw new Exception("There can't be any duplicate in the order");
-            }
-
-            // Check if the beer is sold by the wholesaler and if there's enough stock
-            var beerStocks = await _beerStockRepository.GetAllAsync();
-            var beerStock = beerStocks.FirstOrDefault(x => x.BeerId == addOrderDetailRequestDto.BeerId &&
-                x.WholesalerId == addOrderDetailRequestDto.WholesalerId);
-            if(beerStock is null)
-            {
-                throw new Exception("The beer must be sold by the wholesaler");
-            }
-            else
-            {
-                if (beerStock.StockLeft < addOrderDetailRequestDto.Quantity)
-                {
-                    throw new Exception("The number of beers ordered cannot be greater than the wholesaler's stock");
-                }
             }
         }
     }
---
+++ WholesaleBeer.API/Helpers/OrderDetailValidator.cs
@@ -0,0 +1,62 @@
+﻿using WholesaleBeer.API.Models.DTO;
+using WholesaleBeer.API.Repositories.Interfaces;
+
+namespace WholesaleBeer.API.Helpers
+{
+    public class OrderDetailValidator
+    {
+        private readonly IWholesalerRepository _wholesalerRepository;
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        private readonly IBeerStockRepository _beerStockRepository;
+
+        public OrderDetailValidator(IWholesalerRepository wholesalerRepository,
+            IOrderDetailRepository orderDetailRepository, IBeerStockRepository beerStockRepository)
+        {
+            _wholesalerRepository = wholesalerRepository;
+            _orderDetailRepository = orderDetailRepository;
+            _beerStockRepository = beerStockRepository;
+        }
+
+        public async Task ValidateOrderDetail(AddOrderDetailRequestDto addOrderDetailRequestDto)
+        {
+            // Check if order is empty
+            if (addOrderDetailRequestDto.Quantity <= 0)
+            {
+                throw new Exception("The order cannot be empty");
+            }
+
+            // Check if wholesaler exists
+            var wholesalers = await _wholesalerRepository.GetAllAsync();
+            var wholesaler = wholesalers?.FirstOrDefault(x => x.Id == addOrderDetailRequestDto.WholesalerId);
+            if (wholesaler is null)
+            {
+                throw new Exception("The wholesaler must exist");
+            }
+
+            // Check if there's any duplicate in the order
+            var orderDetails = await _orderDetailRepository.GetAllAsync();
+            var orderDetail = orderDetails?.FirstOrDefault(x => x.BeerId == addOrderDetailRequestDto.BeerId &&
+                x.WholesalerId == addOrderDetailRequestDto.WholesalerId && x.Quantity == addOrderDetailRequestDto.Quantity);
+            if (orderDetail is not null)
+            {
+                throw new Exception("There can't be any duplicate in the order");
+            }
+
+            // Check if the beer is sold by the wholesaler and if there's enough stock
+            var beerStocks = await _beerStockRepository.GetAllAsync();
+            var beerStock = beerStocks?.FirstOrDefault(x => x.BeerId == addOrderDetailRequestDto.BeerId &&
+                x.WholesalerId == addOrderDetailRequestDto.WholesalerId);
+            if (beerStock is null)
+            {
+                throw new Exception("The beer must be sold by the wholesaler");
+            }
+            else
+            {
+                if (beerStock.StockLeft < addOrderDetailRequestDto.Quantity)
+                {
+                    throw new Exception("The number of beers ordered cannot be greater than the wholesaler's stock");
+                }
+            }
+        }
+    }
+}
